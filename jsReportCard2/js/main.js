d98636d5ebfde33c981d96c1e90494034654aa90
Jayant Katia <jayantkatia65@gmail.com>
2020-05-08 12:38:31 +0530

    features added

--- jsReportCard2/js/main.js
+++ jsReportCard2/js/main.js
@@ -1,44 +1,122 @@
-function doCalc(task){
-    let mCpp=Number(document.frm.marksCpp.value);
-    let mJava=Number(document.frm.marksJava.value);
-    let mWeb=Number(document.frm.marksWeb.value);
-    let mApp=Number(document.frm.marksApp.value);
-    let mIoT=Number(document.frm.marksIot.value);
-    let total=mCpp+mIoT+mJava+mWeb+mApp;
-    if(total>500)
-        {
-            alert("-_-  Shi Marks bharo !!");
-            doClear();
-        }
-    else if(task=="sum")
-    document.frm.total.value=total;
-    else if(task=="grade"){
-        let grade;
-        
-        if(total>400)
-        grade= "A";
-        else if(total>300)
-        grade= "B";
-        else if(total>200)
-        grade= "C";
-        else
-        grade= "D";
-
-        document.frm.grade.value=grade;
+//TODO    1. flag  2.solve many documentbyID prob
+//Globals ///////////////////////////////////////////////
+let obj = {
+    identity: {},
+    marks: {}
+};
+//To validate user input (marks out of 100) ///////////////
+function doCheck(id) {
+    let marks = Number(document.getElementById(id).value);
+    let spanReference = document.getElementById(id + "Span");
+    let localFlag = true;  //local jasoos
+    if (marks == "") {
+        spanReference.innerHTML = `Pls fill ${id}`;
+        localFlag = false;
     }
-    else{
-        document.frm.perc.value=total*0.2;
+    if (marks > 100 || marks < 0) {
+        spanReference.innerHTML = ` Check input.  0&lt;marks&lt;100`;
+        localFlag = false;
+    }
+    if (isNaN(marks)) {
+        spanReference.innerHTML = `NaN error. Pls fill valid number`;
+        localFlag = false;
+    }
+    if (localFlag == true) {
+        spanReference.innerHTML = "";
+        obj.identity[`${id}`] = true;
+        obj.marks[`${id}`] = marks;
+    } else {
+        obj.identity[`${id}`] = false;
     }
 }
-function doClear(){
-    document.frm.marksCpp.value="";
-    document.frm.marksJava.value="";
-    document.frm.marksApp.value="";
-    document.frm.marksWeb.value="";
-    document.frm.marksIot.value="";
-    document.frm.name.value="";
-    document.frm.rollNo.value="";
-    document.frm.perc.value="";
-    document.frm.total.value="";
-    document.frm.grade.value="";
+//Task Handler //////////////////////////////////////////////
+function doCalc(task) {
+    let localFlag;
+    for (let key in obj.identity) {
+        if (obj.identity[`${key}`] == false) {
+            localFlag = false;
+            break;
+        }
+        localFlag = true;
+    }
+    if (localFlag) {
+        switch (task) {
+            case "sum":
+                document.getElementById("total").value = doSum();
+                document.getElementById("perc").disabled = false;
+                document.getElementById("perc").readOnly = true;
+
+                break;
+            case "perc":
+                document.getElementById("perc").value = doPerc();
+                document.getElementById("grade").disabled = false;
+                document.getElementById("grade").readOnly=true;
+                break;
+            case "grade":
+                document.getElementById("grade").value = doGrade();
+                break;
+            case "pass":
+                doPass();
+                break;
+            default:
+                alert("The given function isnt added to switch. Please add...")
+        }
+    }
+}
+//Calculation handlers ////////////////////////////////////////////
+const doSum = function () {
+    // document.getElementById()
+    let total = 0;
+    for (let key in obj.marks) {
+        total += obj.marks[`${key}`];
+    }
+    return total;
+}
+const doPerc = () => {
+    let total = doSum();
+    let number = Object.keys(obj.marks).length;
+    return total / number;
+}
+const doGrade = () => {
+    let perc = doPerc();
+    let grade;
+    if (perc > 80)
+        grade = "A";
+    else if (perc > 60)
+        grade = "B";
+    else if (perc > 40)
+        grade = "C";
+    else
+        grade = "X";
+    return grade;
+}
+//Check Pass function////////////////////////////////////////
+function doPass() {
+    let localFlag;
+    for (let key in obj.marks) {
+        if (obj.marks[key] < 40) {
+            localFlag = false;
+            break;
+        }
+        localFlag = true;
+    }
+    if (localFlag)
+        alert("You Passed");
+    else
+        alert("Better Luck Next Time");    
+}
+//Clear function//////////////////////////////////////////////////
+function doClear() {
+    document.getElementById("marksCpp").value = "";
+    document.getElementById("marksJava").value = "";
+    document.getElementById("marksWeb").value = "";
+
+    document.getElementById("rollNo").value = "";
+    document.getElementById("perc").value = "";
+    document.getElementById("total").value = "";
+    document.getElementById("grade").value = "";
+
+    document.getElementById("grade").disabled = true;
+    document.getElementById("perc").disabled = true;
+    document.getElementById("total").disabled = true;
 }
