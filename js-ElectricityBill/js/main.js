82185412f19367159aa0afc72f4e4ebb0f38a8a6
Jayant Katia <jayantkatia65@gmail.com>
2020-05-11 12:05:37 +0530

    clean

--- js-ElectricityBill/js/main.js
+++ js-ElectricityBill/js/main.js
@@ -1,3 +1,10 @@
+//Units
+function doUnits(units) {
+    obj.flagUnits = true;
+    obj["units"] = units.value;
+    document.getElementById("unitSpan").innerHTML = "";
+}
+//Checkboxes
 function doCheck(ele, qty) {
     if (ele.checked == true) {
         qty.disabled = false;
@@ -9,16 +16,13 @@ function doCheck(ele, qty) {
         obj[qty.id] = 0;
     }
 }
-function doUnits(units) {
-    obj.flagUnits = true;
-    obj["units"] = units.value;
-    document.getElementById("unitSpan").innerHTML = "";
 
-}
 const obj = {};
 function qtyFunct(e) {
     obj[e.target.id] = e.target.value;
 }
+
+//Radio buttons
 function radDiscount() {
     obj.flagDiscount = true;
     let residential = document.getElementById("residential");
@@ -29,8 +33,9 @@ function radDiscount() {
         obj["discount"] = 10;
     }
     document.getElementById("radSpan").innerHTML = "";
-
 }
+
+//task handler
 function doCalc(task) {
     if (obj.flagDiscount && obj.flagUnits) {
         switch (task) {
@@ -46,50 +51,50 @@ function doCalc(task) {
                 doNet();
                 break;
         }
-    } else {
-        if (!obj.flagUnits)
-            document.getElementById("unitSpan").innerHTML = "Pls fill units";
-        else {
-            document.getElementById("radSpan").innerHTML = "Pls select one";
-        }
-    }
+    } else if (!obj.flagUnits)
+        document.getElementById("unitSpan").innerHTML = "Pls fill units";
+    else
+        document.getElementById("radSpan").innerHTML = "Pls select one";
 }
-let net=0,discount;
+
+//Calculations handlers
+let net = 0, discount;
+
 function doBill() {
     let cost;
-
     cost = obj.units * 5;
     appendInator("Units bill :  Rs " + cost);
     net += cost;
 
     cost = obj.ACnum * 100;
-    if (! isNaN(cost)) {
+    if (!isNaN(cost)) {
         appendInator("AC Bill :  Rs " + cost);
         net += cost;
     }
-
     cost = obj.CoolerNum * 80;
-    if (! isNaN(cost)) {
+    if (!isNaN(cost)) {
         appendInator("Cooler Bill :  Rs " + cost)
         net += cost;
     }
-
     cost = obj.FanNum * 50;
-    if (! isNaN(cost)) {
+    if (!isNaN(cost)) {
         appendInator("Fan Bill :  Rs " + cost);
         net += cost;
     }
-
     appendInator(" Bill :  Rs " + net);
 }
-function doDiscount(){
-    discount=obj.discount/100*net;
-    appendInator("Discount : Rs "+discount);
+
+function doDiscount() {
+    discount = obj.discount / 100 * net;
+    appendInator("Discount : Rs " + discount);
 }
-function doNet(){
-    let total=net-discount;
-    appendInator("Total Bill : Rs "+total);
+
+function doNet() {
+    let total = net - discount;
+    appendInator("Total Bill : Rs " + total);
 }
+
+//display appendinator
 function appendInator(str) {
     let para = document.createElement("p");
     let text = document.createTextNode(str);
